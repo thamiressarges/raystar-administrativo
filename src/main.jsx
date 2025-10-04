2cd85a79e32724db89a1b0808578cad95b780c2c
Thamires <thamires.sarges.58@gmail.com>
2025-10-04 12:26:44 -0300

    feat: configurar styled-components com tema e estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from './styles/global';
+import {theme} from './styles/theme';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <SignIn />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,10 @@
+export default {
+  COLORS: {
+    BLACK: "#000000",
+    WHITE: "#FFFFFF",
+    GRAY_100: "#E9E9E9",
+    RED: "#AE0606",
+    GREEN: "#06AE33",
+    BLUE: "#086DF0"
+  }
+}
