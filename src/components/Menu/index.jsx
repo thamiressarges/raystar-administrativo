f8b77bceed945b988415798d6f7ee90a0e0acd4d
Thamires <thamires.sarges.58@gmail.com>
2025-11-30 14:23:58 -0300

    Refatorando

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,11 +1,15 @@
+import { 
+    FaFileAlt, 
+    FaUsers, 
+    FaFolder, 
+    FaShoppingCart, 
+    FaCog, 
+    FaUser 
+} from 'react-icons/fa';
+
 import { Container, MenuList } from './styles';
 import { MenuItem } from '../MenuItem';
 import { useMenu } from '../../contexts/MenuContext';
-
-import { 
-    FaFileAlt, FaUsers, FaFolder, 
-    FaShoppingCart, FaCog, FaUser 
-} from 'react-icons/fa';
 
 export function Menu(){
     const { isMenuOpen } = useMenu();
@@ -13,12 +17,54 @@ export function Menu(){
     return(
         <Container $isopen={isMenuOpen}>
             <MenuList>
-                <li><MenuItem title="Pedidos" icon={<FaFileAlt/>} to={'/order'} isMenuOpen={isMenuOpen}/></li>
-                <li><MenuItem title="Clientes" icon={<FaUsers/>} to={'/clients'} isMenuOpen={isMenuOpen}/></li>
-                <li><MenuItem title="Categorias" icon={<FaFolder/>} to={'/category'} isMenuOpen={isMenuOpen}/></li>
-                <li><MenuItem title="Produtos" icon={<FaShoppingCart/>} to={'/products'} isMenuOpen={isMenuOpen}/></li>
-                <li><MenuItem title="Configurações" icon={<FaCog/>} to={'/settings'} isMenuOpen={isMenuOpen}/></li>
-                <li><MenuItem title="Perfil" icon={<FaUser/>} to={'/profile'} isMenuOpen={isMenuOpen}/></li>
+                <li>
+                    <MenuItem 
+                        title="Pedidos" 
+                        icon={<FaFileAlt/>} 
+                        to="/order" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
+                <li>
+                    <MenuItem 
+                        title="Clientes" 
+                        icon={<FaUsers/>} 
+                        to="/clients" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
+                <li>
+                    <MenuItem 
+                        title="Categorias" 
+                        icon={<FaFolder/>} 
+                        to="/category" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
+                <li>
+                    <MenuItem 
+                        title="Produtos" 
+                        icon={<FaShoppingCart/>} 
+                        to="/products" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
+                <li>
+                    <MenuItem 
+                        title="Configurações" 
+                        icon={<FaCog/>} 
+                        to="/settings" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
+                <li>
+                    <MenuItem 
+                        title="Perfil" 
+                        icon={<FaUser/>} 
+                        to="/profile" 
+                        isMenuOpen={isMenuOpen}
+                    />
+                </li>
             </MenuList>
         </Container>
     )
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -2,20 +2,22 @@ import styled from "styled-components";
 
 export const Container = styled.aside`
     grid-area: menu;
-    
-    background-color: ${({theme}) => theme.COLORS.BLACK};
-
+    background-color: ${({theme}) => theme.COLORS.PRIMARY};
     padding-top: 40px;
-
     overflow: hidden;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
 `;
 
 export const MenuList = styled.ul`
     list-style: none;
     padding: 0;
     margin: 0;
+    width: 100%;
 
     > li {
-        margin-bottom: 20px;
+        width: 100%;
+        margin-bottom: 8px;
     }
 `;
