0909384fd5a1da9e69078ef84224318d32193868
Thamires <thamires.sarges.58@gmail.com>
2025-11-30 14:36:03 -0300

    Refatorando

--- src/components/SelectInput/index.jsx
+++ src/components/SelectInput/index.jsx
@@ -1,16 +1,16 @@
 import { Container } from "./styles";
 
 export function SelectInput({ placeholder, options = [], ...rest }) {
-  return (
-    <Container>
-      <select {...rest}>
-        <option value="">{placeholder}</option>
-        {options.map((opt, index) => (
-          <option key={index} value={opt}>
-            {opt}
-          </option>
-        ))}
-      </select>
-    </Container>
-  );
+    return (
+        <Container>
+            <select {...rest} defaultValue="">
+                <option value="" disabled hidden>{placeholder}</option>
+                {options.map((opt, index) => (
+                    <option key={index} value={opt}>
+                        {opt}
+                    </option>
+                ))}
+            </select>
+        </Container>
+    );
 }
