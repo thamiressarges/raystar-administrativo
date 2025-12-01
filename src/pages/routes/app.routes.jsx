6c25c7553a5ae5173615833b2150ad5455680c0a
Thamires <thamires.sarges.58@gmail.com>
2025-12-01 12:20:24 -0300

    Refatorando

--- src/pages/routes/app.routes.jsx
+++ src/pages/routes/app.routes.jsx
@@ -1,36 +1,63 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+// Autenticação
 import { SignIn } from "../SignIn";
-import { ForgotPassword } from "../ForgotPassword"; 
-import { Order } from "../Order"; 
-import { Clients } from "../Clients"; 
-import { Category } from "../Category";
-import { Products } from "../Products";
-import { Settings } from "../Settings";
-import { OrderDetails } from "../OrderDetails";
-import { ProductsDetails } from '../ProductsDetails';
-import {CategoryDetails} from '../CategoryDetails';
-import { NewProduct } from '../NewProduct'
+import { ResetPassword } from "../ResetPassword";
+
+// Core
 import { Profile } from "../Profile";
+import { Settings } from "../Settings";
 import { PrivateRoute } from "../../components/PrivateRoute/PrivateRoute";
 
-export function AppRoutes(){
-  return(
-    <Routes>
-      {/* Rotas públicas */}
-      <Route path="/" element={<SignIn />} />
-      <Route path="/forgotpassword" element={<ForgotPassword />} />
+// Pedidos
+import { Order } from "../Order";
+import { OrderDetails } from "../OrderDetails";
 
-      {/* Rotas privadas */}
+// Clientes
+import { Clients } from "../Clients";
+import { ClientsDetails } from "../ClientsDetails";
+
+// Categorias
+import { Category } from "../Category";
+import { CategoryDetails } from "../CategoryDetails";
+
+// Produtos
+import { Products } from "../Products";
+import { ProductsDetails } from "../ProductsDetails";
+import { NewProduct } from "../NewProduct";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      {/* --- Rotas Públicas --- */}
+      <Route path="/" element={<SignIn />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+
+      {/* --- Rotas Privadas --- */}
+      
+      {/* Pedidos */}
       <Route path="/order" element={<PrivateRoute><Order /></PrivateRoute>} />
+      <Route path="/orderDetails/:id" element={<PrivateRoute><OrderDetails /></PrivateRoute>} />
+
+      {/* Clientes */}
       <Route path="/clients" element={<PrivateRoute><Clients /></PrivateRoute>} />
+      <Route path="/clientsDetails/:id" element={<PrivateRoute><ClientsDetails /></PrivateRoute>} />
+
+      {/* Categorias */}
       <Route path="/category" element={<PrivateRoute><Category /></PrivateRoute>} />
-      <Route path="/products" element={<PrivateRoute><Products /></PrivateRoute>} />
-      <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
-      <Route path="/orderDetails" element={<PrivateRoute><OrderDetails /></PrivateRoute>} />
       <Route path="/categoryDetails/:id" element={<PrivateRoute><CategoryDetails /></PrivateRoute>} />
-      <Route path="/productDetails/:id" element={<PrivateRoute><ProductsDetails></ProductsDetails></PrivateRoute>} />
+
+      {/* Produtos */}
+      <Route path="/products" element={<PrivateRoute><Products /></PrivateRoute>} />
       <Route path="/products/new" element={<PrivateRoute><NewProduct /></PrivateRoute>} />
-      <Route path="/profile" element={<PrivateRoute><Profile/></PrivateRoute>}/>
+      <Route path="/productDetails/:id" element={<PrivateRoute><ProductsDetails /></PrivateRoute>} />
+
+      {/* Configurações e Perfil */}
+      <Route path="/settings" element={<PrivateRoute><Settings /></PrivateRoute>} />
+      <Route path="/profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+
+      {/* Redirecionamento para rotas inexistentes */}
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
-  )
+  );
 }
